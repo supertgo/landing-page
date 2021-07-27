d33ce65072eb75fb7e2f5dfefbcb7a1c6d9b8b8e
supertgo <thiagoroberto2003@gmail.com>
2021-07-27 11:15:22 -0300

    Turned header dynamic

--- web/src/graphql/queries/getLandingPage.ts
+++ web/src/graphql/queries/getLandingPage.ts
@@ -1,10 +1,30 @@
 const GET_LANDING_PAGE = /* GraphQl */ `
-  query GET_LANDING_PAGE {
-    landingPage {
-      logo {
+  fragment logo on LandingPage {
+    logo {
+      alternativeText
+      url
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
+        url
+      }
+      image {
         alternativeText
         url
       }
+    }
+  }
+
+  query GET_LANDING_PAGE {
+    landingPage {
+      ...logo
+      ...header
     }
   }
 `;
