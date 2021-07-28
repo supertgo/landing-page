0e8f35cf790f1ecb45e5d245cbc7e8fd25717bf5
supertgo <thiagoroberto2003@gmail.com>
2021-07-28 11:42:48 -0300

    Turning sectionTec  dynamic

--- web/src/components/SectionTech/index.tsx
+++ web/src/components/SectionTech/index.tsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-import Heading from 'components/Heading'
-import Container from 'components/Container'
+import Heading from 'components/Heading';
+import Container from 'components/Container';
 
-import icons from './content'
-import * as S from './styles'
+import * as S from './styles';
 
-const SectionTech = () => (
+import { SectionTechProps } from 'types/api';
+import { getImageUrl } from 'utils/getImageUrl';
+
+const SectionTech = ({ title, techIcons }: SectionTechProps) => (
   <S.Wrapper>
     <Container>
-      <Heading reverseColor>Tecnologias utilizadas</Heading>
+      <Heading reverseColor>{title}</Heading>
       <S.IconsContainer>
-        {icons.map(({ name, image }) => (
-          <S.Icon key={name}>
-            <S.Icons src={`img/tech/${image}`} alt={name} loading="lazy" />
-            <S.IconsName>{name}</S.IconsName>
+        {techIcons.map(({ title, icon }) => (
+          <S.Icon key={title}>
+            <S.Icons src={getImageUrl(icon.url)} alt={title} loading="lazy" />
+            <S.IconsName>{title}</S.IconsName>
           </S.Icon>
         ))}
       </S.IconsContainer>
     </Container>
   </S.Wrapper>
-)
+);
 
-export default SectionTech
+export default SectionTech;
--- web/src/graphql/queries/getLandingPage.ts
+++ web/src/graphql/queries/getLandingPage.ts
@@ -32,11 +32,24 @@ fragment sectionAboutProject on LandingPage {
   }
 }
 
+fragment sectionTech on LandingPage {
+  sectionTech {
+    title
+    techIcons {
+      icon {
+        url
+      }
+      title
+    }
+  }
+}
+
 query GET_LANDING_PAGE {
   landingPage {
     ...logo
     ...header
     ...sectionAboutProject
+    ...sectionTech
   }
 }
 `;
